29c759fbf131846ca9e702054c196aaf3160125b
sna <sverre.nader@conceptpeople.de>
2016-08-03 13:46:11 +0200

    Fixes #2 - Program sleeps now 2 seconds between the messages

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -2,6 +2,7 @@ public class HelloWorld {
     
     public static void main(String[] args) {
         System.out.println("Hello world");
+        try { Thread.sleep(2000); } catch (Exception e) { e.printStackTrace(); }
         System.out.println("Bye world");
     }
     
